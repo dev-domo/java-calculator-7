91be335cd698472604cbc1f07d7b3118e1e45cd1
pakaheo <k70923@konkuk.ac.kr>
2024-12-01 15:57:56 +0900

    fix : 숫자 단일 입력이 오류 처리되지 않도록 보완

--- src/main/java/calculator/util/StringUtil.java
+++ src/main/java/calculator/util/StringUtil.java
@@ -5,13 +5,17 @@ import calculator.enums.ExceptionMessage;
 public class StringUtil {
     public static String[] splitText(String text, String delimiter) {
         String[] splitted = text.split(delimiter);
-        checkInvalidDelimiter(splitted[0], text);
+        checkInvalidDelimiter(splitted, text);
         return splitted;
     }
 
-    private static void checkInvalidDelimiter(String splittedText, String originalText) {
-        if (splittedText.equals(originalText)) {
+    private static void checkInvalidDelimiter(String[] splitted, String originalText) {
+        if (isNotSplitted(splitted, originalText)) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT.valueOf());
         }
+    }
+
+    private static boolean isNotSplitted(String[] splitted, String originalText) {
+        return splitted.length > 1 && splitted[0].equals(originalText);
     }
 }
