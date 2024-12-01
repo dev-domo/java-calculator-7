a5b5ca8baccb322565a1d333305a3331b5d5b59a
pakaheo <k70923@konkuk.ac.kr>
2024-12-01 14:45:44 +0900

    refactor : 계산기를 싱글턴으로 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -2,7 +2,9 @@ package calculator.domain;
 
 import java.util.List;
 
-public class Calculator {
+public enum Calculator {
+    CALCULATOR;
+
     public int calculate(List<Integer> numbers) {
         return numbers.stream().reduce(0, Integer::sum);
     }
--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
     @Test
     void 숫자_리스트_덧셈() {
-        Calculator calculator = new Calculator();
-
-        assertThat(calculator.calculate(List.of(1, 2, 3))).isEqualTo(6);
+        assertThat(Calculator.CALCULATOR.calculate(List.of(1, 2, 3))).isEqualTo(6);
     }
 }
