0fbb99dacf5eb539a8fcb624f5ece0f809ca8223
pakaheo <k70923@konkuk.ac.kr>
2024-12-01 13:58:29 +0900

    feat : null이나 빈 문자열 처리

--- src/main/java/calculator/domain/EmptyInputType.java
+++ src/main/java/calculator/domain/EmptyInputType.java
@@ -1,2 +1,16 @@
-package calculator.domain;public class EmptyInputType {
+package calculator.domain;
+
+import java.util.List;
+
+public class EmptyInputType implements InputType {
+    private final String input;
+
+    public EmptyInputType(String input) {
+        this.input = input;
+    }
+
+    @Override
+    public List<Integer> extractNumbers() {
+        return null;
+    }
 }
