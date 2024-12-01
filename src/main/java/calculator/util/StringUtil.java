4983a7202df0ad8d40d507a0d2887e1e812badd3
pakaheo <k70923@konkuk.ac.kr>
2024-12-01 15:43:30 +0900

    feat : 문자열에 포함된 구분자와 나누려는 구분자가 다른 경우

--- src/main/java/calculator/util/StringUtil.java
+++ src/main/java/calculator/util/StringUtil.java
@@ -1,7 +1,17 @@
 package calculator.util;
 
+import calculator.enums.ExceptionMessage;
+
 public class StringUtil {
     public static String[] splitText(String text, String delimiter) {
-        return text.split(delimiter);
+        String[] splitted = text.split(delimiter);
+        checkInvalidDelimiter(splitted[0], text);
+        return splitted;
+    }
+
+    private static void checkInvalidDelimiter(String splittedText, String originalText) {
+        if (splittedText.equals(originalText)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT.valueOf());
+        }
     }
 }
--- src/test/java/calculator/util/StringUtilTest.java
+++ src/test/java/calculator/util/StringUtilTest.java
@@ -1,12 +1,21 @@
 package calculator.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import calculator.enums.ExceptionMessage;
 import org.junit.jupiter.api.Test;
 
 public class StringUtilTest {
     @Test
-    void split() {
+    void 구분자를_기준으로_문자열_나누기() {
         assertThat(StringUtil.splitText("1,2,3", ",")).containsExactly("1", "2", "3");
+    }
+
+    @Test
+    void 문자열에_포함된_구분자와_split_기준이_되는_구분자가_서로_다르면_예외() {
+        assertThatThrownBy(() -> StringUtil.splitText("1,2,3", ":"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.INVALID_INPUT.valueOf());
     }
 }
