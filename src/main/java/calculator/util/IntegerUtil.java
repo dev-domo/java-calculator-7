1a636b1f0accd87482ed9c2334bcfc55500a869f
pakaheo <k70923@konkuk.ac.kr>
2024-12-01 15:34:52 +0900

    feat : 숫자로 변환할 수 없는 문자 예외 처리

--- src/main/java/calculator/util/IntegerUtil.java
+++ src/main/java/calculator/util/IntegerUtil.java
@@ -1,7 +1,13 @@
 package calculator.util;
 
+import calculator.enums.ExceptionMessage;
+
 public class IntegerUtil {
     public static int parseText(String text) {
-        return Integer.parseInt(text);
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT.valueOf());
+        }
     }
 }
--- src/test/java/calculator/util/IntegerUtilTest.java
+++ src/test/java/calculator/util/IntegerUtilTest.java
@@ -1,12 +1,21 @@
 package calculator.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import calculator.enums.ExceptionMessage;
 import org.junit.jupiter.api.Test;
 
 public class IntegerUtilTest {
     @Test
     void 문자를_숫자로_변환() {
         assertThat(IntegerUtil.parseText("3")).isEqualTo(3);
+    }
+
+    @Test
+    void 숫자로_변환할_수_없는_문자인_경우_예외() {
+        assertThatThrownBy((() -> IntegerUtil.parseText("가")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.INVALID_INPUT.valueOf());
     }
 }
