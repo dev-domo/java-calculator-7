3d2b0ac8ff545f1fdcd32b54a20fcda723954d5e
pakaheo <k70923@konkuk.ac.kr>
2024-12-01 13:58:15 +0900

    feat : 커스텀 구분자 문자열 처리

--- src/main/java/calculator/domain/CustomInputType.java
+++ src/main/java/calculator/domain/CustomInputType.java
@@ -1,2 +1,16 @@
-package calculator.domain;public class CustomInputType {
+package calculator.domain;
+
+import java.util.List;
+
+public class CustomInputType implements InputType {
+    private final String input;
+
+    public CustomInputType(final String input) {
+        this.input = input;
+    }
+
+    @Override
+    public List<Integer> extractNumbers() {
+        return null;
+    }
 }
